c86cfffe88f3ba5e9b7e0306bbb4dc4a09e84db3
guardian-bear <rob@guardianbear.net>
2022-08-21 10:40:17 -0700

    Basic addition calculator

---
+++ basic_add_calc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_add_calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Input: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Second Number Input: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+    }
+}
